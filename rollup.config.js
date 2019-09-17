ffc2f01047ad68be623e468c50ab34efc56779d3
Kevin <kevinbe71@gmail.com>
2019-09-16 23:50:24 -0400

    prevent .css.d.ts files + remove app css.d.ts file

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import postcss from 'rollup-plugin-postcss-modules';
 import typescript from 'rollup-plugin-typescript2';
 import autoprefixer from 'autoprefixer';
 import jsx from 'rollup-plugin-jsx';
-// import postcss from 'rollup-plugin-postcss';
-// import css from "@modular-css/rollup";
 import image from 'rollup-plugin-image';
 import json from 'rollup-plugin-json';
 
@@ -25,17 +23,10 @@ export default {
         ...Object.keys(pkg.dependencies || {}),
         ...Object.keys(pkg.peerDependencies || {}),
     ], plugins: [
-        // import postcss from 'rollup-plugin-postcss';
-        // postcss({
-        //     include: '*.module.css',
-        //     modules: true,
-        //     // Or with custom options for `postcss-modules`
-        //     modules: {}
-        // }),
         postcss({
             extract: true,
             plugins: [autoprefixer()],
-            writeDefinitions: true
+            writeDefinitions: false
         }),
         typescript({
             typescript: require('typescript'),
@@ -45,7 +36,6 @@ export default {
         json({
             compact: true
         }),
-        // css(),
         jsx({ factory: 'React.createElement' }),
         image()
     ],
--- src/App.module.css.d.ts
+++
@@ -1,6 +0,0 @@
-export const app: string
-interface Namespace {
-	"app": string,
-}
-declare const appModule: Namespace
-export default appModule
