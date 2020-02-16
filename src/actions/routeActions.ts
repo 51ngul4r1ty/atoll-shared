f38e7a78d8cab0fea84c3418c2028fd6653d8c28
Kevin <kevinbe71@gmail.com>
2020-02-15 22:34:17 -0500

    fixed useDispatch related issue + adding routing

---
+++ src/actions/routeActions.ts
@@ -0,0 +1,14 @@
+// consts/enums
+import * as ActionTypes from "./actionTypes";
+
+export const routePlanView = () => ({
+    type: ActionTypes.ROUTE_PLAN_VIEW
+});
+
+export const routeSprintView = () => ({
+    type: ActionTypes.ROUTE_SPRINT_VIEW
+});
+
+export const routeReviewView = () => ({
+    type: ActionTypes.ROUTE_REVIEW_VIEW
+});
---
+++ src/middleware/routingMiddleware.ts
@@ -0,0 +1,21 @@
+// externals
+import { Action } from "redux";
+import { push } from "connected-react-router";
+
+// consts/enums
+import * as ActionTypes from "../actions/actionTypes";
+
+export const routingMiddleware = ({ dispatch }) => (next) => (action: Action) => {
+    next(action);
+    switch (action.type) {
+        case ActionTypes.ROUTE_PLAN_VIEW:
+            dispatch(push("/plan"));
+            break;
+        case ActionTypes.ROUTE_SPRINT_VIEW:
+            dispatch(push("/sprint"));
+            break;
+        case ActionTypes.ROUTE_REVIEW_VIEW:
+            dispatch(push("/review"));
+            break;
+    }
+};
