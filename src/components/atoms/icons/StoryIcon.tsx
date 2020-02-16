c1a1974a453491017923c34c43c2778e96a1294b
Kevin <kevinbe71@gmail.com>
2020-02-15 19:40:50 -0500

    fixed various issues

--- src/components/atoms/icons/StoryIcon.tsx
+++ src/components/atoms/icons/StoryIcon.tsx
@@ -10,14 +10,14 @@ export const StoryIcon: React.FC<{}> = () => (
     <>
         <svg
             fill="none"
-            fill-rule="evenodd"
+            fillRule="evenodd"
             className={baseCss.strokeInverted}
             stroke="black"
-            stroke-width="0.501"
-            stroke-linejoin="bevel"
-            stroke-miterlimit="10"
-            font-family="Times New Roman"
-            font-size="16"
+            strokeWidth="0.501"
+            strokeLinejoin="bevel"
+            strokeMiterlimit="10"
+            fontFamily="Times New Roman"
+            fontSize="16"
             style={{ fontVariantLigatures: "none" }}
             xmlnsXlink="http://www.w3.org/1999/xlink"
             xmlns="http://www.w3.org/2000/svg"
@@ -33,12 +33,12 @@ export const StoryIcon: React.FC<{}> = () => (
                     d="M 114.498,31.75 L 4.417,224.441 L 183.626,189.721 L 225.808,31.75 L 114.498,31.75 Z M 226.322,31.75 L 265.416,190.387 L 444.695,228.024 L 264.675,262.903 L 222.599,420.54 L 336.672,420.54 L 447.75,226.136 L 336.654,31.75 L 226.322,31.75 Z M 4.25,224.734 L 3.439,226.154 L 114.517,420.54 L 221.915,420.54 L 182.885,262.236 L 4.25,224.734 Z"
                     className={baseCss.fillInverted}
                     fill="#000000"
-                    stroke-linecap="round"
-                    stroke-width="11.108"
-                    stroke-linejoin="miter"
+                    strokeLinecap="round"
+                    strokeWidth="11.108"
+                    strokeLinejoin="miter"
                     stroke="none"
-                    marker-start="none"
-                    marker-end="none"
+                    markerStart="none"
+                    markerEnd="none"
                 />
             </g>
         </svg>
