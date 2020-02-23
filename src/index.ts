34c3c9c1672a86b5edc5ff0874fc0a10bd7725b1
Kevin <kevinbe71@gmail.com>
2020-02-23 11:59:53 -0500

    get basic feature toggle code in place

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,37 @@
-export { App } from "./App";
-export { AppContainer } from "./AppContainer";
+// consts/enums
 export { APPLICATION_JSON } from "./constants";
+
+// components
+export { App } from "./App";
 export { BacklogItemCard, BacklogItemTypeEnum } from "./components/molecules/cards/BacklogItemCard";
 export { EditButton, EditMode } from "./components/molecules/buttons/EditButton";
 export { EditIcon } from "./components/atoms/icons/EditIcon";
 export { HamburgerIcon } from "./components/atoms/icons/HamburgerIcon";
 export { HomeButton } from "./components/molecules/buttons/HomeButton";
-export { PlanViewContainer } from "./PlanViewContainer";
-export { ReviewViewContainer } from "./ReviewViewContainer";
 export { SimpleButton } from "./components/atoms/buttons/SimpleButton";
 export { SimpleText } from "./components/atoms/text/SimpleText";
-export { SprintViewContainer } from "./SprintViewContainer";
 export { TabStrip } from "./components/atoms/tabs/TabStrip";
-export { storeHistoryInstance, getHistoryInstance } from "./config";
+
+// interfaces/types
+export { StateTree, BacklogItem, BacklogItemsState, FeatureToggle, FeatureTogglesState } from "./types";
 
 // containers
-export { configureStore } from "./store";
 export { default as IntlProvider } from "./i18n/IntlProvider";
-import { MainLayout } from "./layouts/MainLayout";
+export { AppContainer } from "./AppContainer";
+export { PlanViewContainer } from "./PlanViewContainer";
+export { ReviewViewContainer } from "./ReviewViewContainer";
+export { SprintViewContainer } from "./SprintViewContainer";
+
+// utils
+export { configureStore } from "./store";
 export { createClientHistory, createServerHistory } from "./store/history";
+export { storeHistoryInstance, getHistoryInstance } from "./config";
 
 // themes
 export { themeList } from "./themes/all";
 
+// layouts
+import { MainLayout } from "./layouts/MainLayout";
 export const layouts = {
     MainLayout
 };
