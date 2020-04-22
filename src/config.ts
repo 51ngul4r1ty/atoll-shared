f35d071327859bf08389b8fdf677b8f877e4439a
Kevin <kevinbe71@gmail.com>
2020-04-21 22:14:16 -0400

    auth config

--- src/config.ts
+++ src/config.ts
@@ -121,3 +121,7 @@ export const parsePostgresUrl = (url: string): DbConfig => {
 export const getDbConfig = (): DbConfig => {
     return parsePostgresUrl(process.env.ATOLL_DATABASE_URL || process.env.DATABASE_URL);
 };
+
+export const getAuthKey = (): string | null => {
+    return process.env.ATOLL_AUTH_KEY || null;
+};
