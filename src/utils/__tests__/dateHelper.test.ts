5fef9a77844e143e32ea472e9659b011176b62ac
Kevin <kevinbe71@gmail.com>
2020-05-01 08:48:39 -0400

    add test

--- src/utils/__tests__/dateHelper.test.ts
+++ src/utils/__tests__/dateHelper.test.ts
@@ -2,7 +2,7 @@
 import "jest";
 
 // code under test
-import { addHours, addMinutes } from "../dateHelper";
+import { addHours, addMinutes, addSeconds } from "../dateHelper";
 
 describe("Date Helper", () => {
     describe("addHours", () => {
@@ -19,6 +19,14 @@ describe("Date Helper", () => {
             const result = addMinutes(startDate, 5.5);
             expect(startDate.toISOString()).toEqual("2005-10-14T09:13:59.900Z");
             expect(result.toISOString()).toEqual("2005-10-14T09:19:29.900Z");
+        });
+    });
+    describe("addSeconds", () => {
+        it("should correctly add 5 1/2 seconds to the date", () => {
+            const startDate = new Date(2005, 9, 14, 5, 13, 59, 900);
+            const result = addSeconds(startDate, 5.5);
+            expect(startDate.toISOString()).toEqual("2005-10-14T09:13:59.900Z");
+            expect(result.toISOString()).toEqual("2005-10-14T09:14:05.400Z");
         });
     });
 });
