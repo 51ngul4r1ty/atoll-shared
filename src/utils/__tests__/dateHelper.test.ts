dcaf354c12a2757af81a0fd2e610af3aa7b9ab48
Kevin <kevinbe71@gmail.com>
2020-05-07 00:18:44 -0400

    fix tests for github test

--- src/utils/__tests__/dateHelper.test.ts
+++ src/utils/__tests__/dateHelper.test.ts
@@ -7,26 +7,26 @@ import { addHours, addMinutes, addSeconds } from "../dateHelper";
 describe("Date Helper", () => {
     describe("addHours", () => {
         it("should correctly add 5 1/2 hours to the date", () => {
-            const startDate = new Date(2005, 9, 14, 5, 13, 59, 900);
+            const startDate = new Date(Date.UTC(2005, 9, 14, 5, 13, 59, 900));
             const result = addHours(startDate, 5.5);
-            expect(startDate.toISOString()).toEqual("2005-10-14T09:13:59.900Z");
-            expect(result.toISOString()).toEqual("2005-10-14T14:43:59.900Z");
+            expect(startDate.toISOString()).toEqual("2005-10-14T05:13:59.900Z");
+            expect(result.toISOString()).toEqual("2005-10-14T10:43:59.900Z");
         });
     });
     describe("addMinutes", () => {
         it("should correctly add 5 1/2 mins to the date", () => {
-            const startDate = new Date(2005, 9, 14, 5, 13, 59, 900);
+            const startDate = new Date(Date.UTC(2005, 9, 14, 5, 13, 59, 900));
             const result = addMinutes(startDate, 5.5);
-            expect(startDate.toISOString()).toEqual("2005-10-14T09:13:59.900Z");
-            expect(result.toISOString()).toEqual("2005-10-14T09:19:29.900Z");
+            expect(startDate.toISOString()).toEqual("2005-10-14T05:13:59.900Z");
+            expect(result.toISOString()).toEqual("2005-10-14T05:19:29.900Z");
         });
     });
     describe("addSeconds", () => {
         it("should correctly add 5 1/2 seconds to the date", () => {
-            const startDate = new Date(2005, 9, 14, 5, 13, 59, 900);
+            const startDate = new Date(Date.UTC(2005, 9, 14, 5, 13, 59, 900));
             const result = addSeconds(startDate, 5.5);
-            expect(startDate.toISOString()).toEqual("2005-10-14T09:13:59.900Z");
-            expect(result.toISOString()).toEqual("2005-10-14T09:14:05.400Z");
+            expect(startDate.toISOString()).toEqual("2005-10-14T05:13:59.900Z");
+            expect(result.toISOString()).toEqual("2005-10-14T05:14:05.400Z");
         });
     });
 });
