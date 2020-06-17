3b7fb0710f5e7b9ab12937f9f53e15a21bd3467c
Kevin Berry <41717340+51ngul4r1ty@users.noreply.github.com>
2020-06-16 22:46:04 -0400

    Story/000108/improve logging (#40)
    * improve logging + fix unique key err
    
    * bump ver
    
    Co-authored-by: Kevin <kevinbe71@gmail.com>

---
+++ src/constants/loggingTags.ts
@@ -0,0 +1,5 @@
+export const KEEPALIVE = "keepalive";
+export const WEBSOCKET = "websocket";
+export const DRAG_BACKLOGITEM = "drag:backlogitem";
+
+export type LoggingTag = typeof KEEPALIVE | typeof WEBSOCKET;
--- src/utils/wsClient.ts
+++ src/utils/wsClient.ts
@@ -1,3 +1,4 @@
+// consts/enums
 // externals
 import { w3cwebsocket as W3CWebSocket, IMessageEvent } from "websocket";
 
@@ -5,7 +6,13 @@ import { w3cwebsocket as W3CWebSocket, IMessageEvent } from "websocket";
 import { getApiHostName, getApiPort, requiresSecureProtocol } from "../config";
 
 // interfaces/types
-import { WebsocketPushNotification, PushNotificationType, BasePushNotification } from "../types";
+import { PushNotificationType, BasePushNotification } from "../types";
+
+// utils
+import * as logger from "./logger";
+
+// consts/enums
+import * as loggingTags from "../constants/loggingTags";
 
 let client: W3CWebSocket;
 
@@ -13,16 +20,16 @@ let keepaliveTimeout: NodeJS.Timeout = null;
 
 const setKeepaliveTimeout = () => {
     keepaliveTimeout = setTimeout(() => {
-        console.log("re-scheduling next keepalive");
+        logger.info("re-scheduling next keepalive", [loggingTags.KEEPALIVE]);
         setKeepaliveTimeout();
         const keepalivePayload: BasePushNotification = {
             type: PushNotificationType.KeepAlive
         };
-        console.log("send keepalive");
+        logger.info("send keepalive", [loggingTags.KEEPALIVE]);
         try {
             send(keepalivePayload);
         } catch (err) {
-            console.warn(`Unable to send keepalive timeout: ${err}`);
+            logger.warn(`Unable to send keepalive timeout: ${err}`, [loggingTags.KEEPALIVE]);
         }
     }, 30000);
 };
@@ -33,19 +40,18 @@ export const init = (callback: { (data: any) }) => {
     const scheme = requiresSecureProtocol() ? "wss" : "ws";
     client = new W3CWebSocket(`${scheme}://${apiHostName}:${apiPort}/ws`);
     client.onopen = () => {
-        console.log("WebSocket Client Connected");
+        logger.info("WebSocket Client Connected", [loggingTags.WEBSOCKET]);
         if (keepaliveTimeout) {
             clearTimeout(keepaliveTimeout);
         }
         setKeepaliveTimeout();
     };
     client.onclose = () => {
-        console.log("WebSocket Client Disconnected");
+        logger.info("WebSocket Client Disconnected", [loggingTags.WEBSOCKET]);
     };
     client.onmessage = (message: IMessageEvent) => {
         callback(message.data);
-        // dispatch(receiveWebSocketMessage(message.data));
-        console.log(`Dispatched websocket message ${message}`);
+        logger.info(`Dispatched websocket message ${message}`, [loggingTags.WEBSOCKET]);
     };
 };
 
