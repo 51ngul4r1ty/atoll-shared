c616c72e1c01840669833c2f27e316fea6cb8e46
Kevin <kevinbe71@gmail.com>
2020-09-08 22:52:48 -0400

    fix test

--- src/reducers/__tests__/appReducer.test.ts
+++ src/reducers/__tests__/appReducer.test.ts
@@ -2,12 +2,12 @@
 import "jest";
 
 // code under test
-import { appReducer, initialState } from "../appReducer";
+import { appReducer, appReducerInitialState } from "../appReducer";
 import * as ActionTypes from "../../actions/actionTypes";
 
 describe("App Reducer", () => {
     it("sets the locale", () => {
-        const actual = appReducer(initialState, { type: ActionTypes.SET_LOCALE, payload: "de_DE" });
+        const actual = appReducer(appReducerInitialState, { type: ActionTypes.SET_LOCALE, payload: "de_DE" });
         expect(actual.locale).toEqual("de_DE");
     });
 });
